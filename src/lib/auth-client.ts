c031d5f77d8ca02467b32143e47c69acc5bb9925
moniraofficial <me.moniraofficial@gmail.com>
2026-07-15 23:09:37 +0600

    fixing error

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
 
-    baseURL: process.env.BETTER_AUTH_URL 
+    baseURL: "https://skillbridge-frontend-iota-one.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient();
